94e9b9541968b00e3f4c1f25c90a743c1041e0ca
GianluGiarde <gianlugiarde@gmail.com>
2023-04-18 18:14:59 +0200

    fix jsnack3 for cicle

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,20 +5,20 @@
 // Prendere gli elementi dell'array in posizione dispari e sommarli OK
 
 const numberList = []
+const amount = 10;
 let sum = 0;
 
 // random number generator 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < amount; i++) {
     let random = Math.floor(Math.random() * 20) + 1;
     numberList.push(random);
 }
-console.log(numberList);
 
-
-// debugger;
-// sum odd position numbers in array
-for (let i = 1; i < 10; i += 2) {
+// sum odd position array numbers
+for (let i = 1; i < amount; i += 2) {
     sum += numberList[i];
 }
+
+console.log(numberList);
 console.log(sum);
 
