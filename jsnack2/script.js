52527d56d7e7c1ca59e58b396fe1cf6eed2f1fc3
GianluGiarde <gianlugiarde@gmail.com>
2023-04-19 10:36:13 +0200

    fix variables

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,9 +1,5 @@
+// 2) snack 2.2
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
-
-// Crea un lista di nomi e una lista di cognomi OK
-// Prendere un elemento casuale da entrambe gli array OK
-// Creare una array dove salvare tutto OK
-// Fare un ciclo dove vengano creati nomi e cognomi da pushare nel nuovo array OK
 
 const nameList = [
     "Bradd",
@@ -34,8 +30,8 @@ const lastNameList = [
 const newList = [];
 
 for (let i = 0; i < 10; i++) {
-    let randomName = Math.floor(Math.random() * ((nameList.length - 1) + 1));
-    let randomLastName = Math.floor(Math.random() * ((nameList.length - 1) + 1));
+    const randomName = Math.floor(Math.random() * ((nameList.length - 1) + 1));
+    const randomLastName = Math.floor(Math.random() * ((nameList.length - 1) + 1));
 
     const finalName = nameList[randomName] + " " + lastNameList[randomLastName];
     newList.push(finalName);
